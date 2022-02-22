8fa07f0132a22383be705aedf58b71927ae2e9ec
unknown <jt.mccutcheon@live.com>
2022-02-22 05:11:18 -0500

    better dates

--- src/components/filters/BusterDates.js
+++ src/components/filters/BusterDates.js
@@ -6,24 +6,28 @@ export const BusterDates = ({
 }) => {
   return (
     <div className="dates">
-      <label>start date: </label>
-      <input
-        className="date_input"
-        placeholder={startDate}
-        value={startDate}
-        onChange={e => setStartDate(e.target.value)}
-        type="date"
-        max={endDate}
-      ></input>
-      <label className="end_label">end date: </label>
-      <input
-        className="date_input"
-        placeholder={endDate}
-        value={endDate}
-        type="date"
-        onChange={e => setEndDate(e.target.value)}
-        min={startDate}
-      ></input>
+      <div className="date_container">
+        <label>start date</label>
+        <input
+          className="date_input"
+          placeholder={startDate}
+          value={startDate}
+          onChange={e => setStartDate(e.target.value)}
+          type="date"
+          max={endDate}
+        />
+      </div>
+      <div className="date_container">
+        <label className="end_label">end date</label>
+        <input
+          className="date_input"
+          placeholder={endDate}
+          value={endDate}
+          type="date"
+          onChange={e => setEndDate(e.target.value)}
+          min={startDate}
+        />
+      </div>
     </div>
   );
 };
