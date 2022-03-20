e318636050ac2d0fbab508d3e35598f5b09b59a4
Justin <justin@hammett.dev>
2022-03-19 19:34:12 -0500

    added crude buster of year functionality

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useQuery } from 'graphql-hooks';
 import { BUSTER_AWARDS } from '../queries';
 import { Loading } from './common/Loading';
@@ -12,12 +12,41 @@ export const Awards = () => {
     variables: { year },
   });
 
+  useEffect(() => {
+    console.log(year)
+
+  }, [year])
+
   // netlify requires everything to be defined and used so placeholder
-  const changeYear = () => setYear('');
+  const changeYear = (year) => setYear(year);
 
   if (loading) return <Loading />;
   if (error) return <Error />;
-
+  console.log(year);
   console.log({ data });
-  return <div onClick={changeYear}>Awards</div>;
+  return (
+  <>
+  <select onChange={(e) => changeYear(e.target.value)} name='year' id='year' value={year} >
+    <option value='2022'>2022</option>
+    <option value='2021'>2021</option>
+  </select>
+
+  <h5>Buster of the year</h5>
+  {data.bustersOTY.length >= 1 &&  data.bustersOTY.map((BOY) => (
+
+    BOY.busters.map((BusterOfYear) => {
+      console.log(BusterOfYear)
+    return (
+    <>
+    <img src={BusterOfYear.avatarUrl} />
+    <p>{`${BusterOfYear.username} with ${BusterOfYear.datesWon.length} busts`}</p>
+    </>
+    )
+  }
+  
+    )
+  ))}
+  
+  </>
+  )
 };
