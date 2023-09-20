b6b1133c33ab4d4b7660b3ed5092b0ca4e4328ed
Jtmccutcheon <jt.mccutcheon@live.com>
2023-09-20 18:36:02 -0400

    update dry page and year scatter tooltip

--- src/components/DryBusters.js
+++ src/components/DryBusters.js
@@ -3,6 +3,7 @@ import { differenceInDays } from 'date-fns';
 import { DEFAULT_QUERY } from '../queries';
 import { Loading } from './common/Loading';
 import { Error } from './common/Error';
+import { Buster } from './season/Buster';
 
 export const DryBusters = () => {
   const { data, loading, error } = useQuery(DEFAULT_QUERY);
@@ -10,21 +11,36 @@ export const DryBusters = () => {
   if (loading) return <Loading />;
   if (error) return <Error />;
   return (
-    <div className="dry_busters">
-      <div className="dry_title">Days Since Last Bust</div>
-      {data.busters.map(b => {
-        return (
-          <div key={b.username}>
-            <div className="dry_days">
-              <div className="dry_names">{b.username}: </div>{' '}
-              {differenceInDays(
+    <div>
+      <h1 className="dry_title">Days Since Last Bust</h1>
+      <div className="dry_busters">
+        {data.busters
+          .sort(
+            (a, b) =>
+              differenceInDays(
                 new Date(),
                 new Date(b.datesWon[b.datesWon.length - 1]),
-              )}
-            </div>
-          </div>
-        );
-      })}
+              ) -
+              differenceInDays(
+                new Date(),
+                new Date(a.datesWon[a.datesWon.length - 1]),
+              ),
+          )
+          .map(b => {
+            return (
+              <div key={b.username}>
+                <div className="dry_days">
+                  <Buster buster={b} width={609} />
+                  {/* <div className="dry_names">{b.username}: </div>{' '} */}
+                  {differenceInDays(
+                    new Date(),
+                    new Date(b.datesWon[b.datesWon.length - 1]),
+                  )}
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
